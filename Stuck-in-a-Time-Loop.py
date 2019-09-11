50526d6c5c0e4d6f02317d155ffb147a89959831
USFMumaAnalyticsTeam <54073789+USFMumaAnalyticsTeam@users.noreply.github.com>
2019-09-11 09:49:33 -0400

    Add files via upload

---
+++ Stuck-in-a-Time-Loop.py
@@ -0,0 +1,5 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 23:09:28) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> n = int(input())
+for i in range(1, n + 1):
+    print(i, 'Abracadabra');
