5ad25a4d580e554d134007e160f7b4b90a0693f3
USFMumaAnalyticsTeam <54073789+USFMumaAnalyticsTeam@users.noreply.github.com>
2019-11-22 05:30:12 -0500

    Add files via upload

---
+++ Hissing-Microphone.py
@@ -0,0 +1,8 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> s = input()
+
+if "ss" in s:
+    print ("hiss")
+else:
+    print ("no hiss")
